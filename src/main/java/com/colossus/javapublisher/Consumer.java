d9c1595dab9add99bc93c2993073c275405f6f27
Kirill Kosykh <triplesonicslash@list.ru>
2023-08-11 12:18:41 +0300

    Consumer added, auto_acknowledge

--- src/main/java/com/colossus/javapublisher/Consumer.java
+++ src/main/java/com/colossus/javapublisher/Consumer.java
@@ -6,10 +6,10 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 public class Consumer {
     public static void main(String[] args) {
         ConnectionFactory factory = new ActiveMQConnectionFactory("admin","admin", "tcp://localhost:61616");
-        try {
-            Connection connection = factory.createConnection();
+
+        try(Connection connection = factory.createConnection()) {
             connection.start();
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
             Destination destination = session.createQueue("demo");
             MessageConsumer consumer = session.createConsumer(destination);
 
@@ -17,12 +17,11 @@ public class Consumer {
                 TextMessage textMessage = (TextMessage) message;
                 try {
                     System.out.println("Consumer got a message: " + textMessage.getText());
+
                 } catch (JMSException e) {
                     throw new RuntimeException(e);
                 }
             });
-
-
         } catch (JMSException e) {
             throw new RuntimeException(e);
         }
